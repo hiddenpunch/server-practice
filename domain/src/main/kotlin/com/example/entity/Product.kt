d69d514112552ca8707e2908641fcfd390a2c9e3
hiddenpunch <superkgd@gmail.com>
2023-01-23 14:14:52 +0900

    feat: make domain entity

---
+++ domain/src/main/kotlin/com/example/entity/Product.kt
@@ -0,0 +1,9 @@
+package com.example.entity
+
+data class Product(
+    val id: String,
+    val descriptions: List<ProductDescription>,
+    val price: Double,
+    val commission: Double,
+    val isExamined: Boolean
+)
